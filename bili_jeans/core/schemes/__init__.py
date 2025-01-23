6b844dca3f40a66b7e20cd6772119e858a9635a0
Chaojie Yan <ushareroses@gmail.com>
2025-01-23 14:01:35 +0800

    test: UGC play cases

--- bili_jeans/core/schemes/__init__.py
+++ bili_jeans/core/schemes/__init__.py
@@ -1,5 +1,5 @@
 """
 Scheme definition for type hint
 """
-from .ugc_play import GetUGCPlayResponse
-from .ugc_view import GetUGCViewResponse
+from .ugc_play import GetUGCPlayResponse  # noqa: F401
+from .ugc_view import GetUGCViewResponse  # noqa: F401
