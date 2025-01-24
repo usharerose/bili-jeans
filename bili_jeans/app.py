9435acf7ddfd4bf09b935534e223a831da4621ec
Chaojie Yan <ushareroses@gmail.com>
2025-01-24 17:33:58 +0800

    feat: support multi-pages resources

--- bili_jeans/app.py
+++ bili_jeans/app.py
@@ -1,39 +1,51 @@
 """
 bili-jeans application
 """
+import asyncio
 from pathlib import Path
 from typing import Optional
 
 from .core.constants import FormatNumberValue
 from .core.download import download_resource
 from .core.factory import parse_web_view_url
-from .core.proxy import get_ugc_play, get_ugc_view
+from .core.pages import get_ugc_pages
+from .core.proxy import get_ugc_play
+from .core.schemes import PageData
 
 
 async def run(
     url: str,
     dir_path: str,
     sess_data: Optional[str] = None
-):
+) -> None:
     dir_p = Path(dir_path)
     assert dir_p.is_dir() is True  # given path should be a directory
 
     metadata = await parse_web_view_url(url)
-    ugc_view = await get_ugc_view(bvid=metadata.bvid, aid=metadata.aid)
-    if ugc_view.data is None:
-        raise
-    ugc_view_data = ugc_view.data
+    pages = await get_ugc_pages(metadata, sess_data)
+
+    tasks = [
+        asyncio.create_task(_download_page(page_data, dir_p, sess_data))
+        for page_data in pages
+    ]
+    _ = await asyncio.gather(*tasks)
+
+
+async def _download_page(
+    page_data: PageData,
+    dir_path: Path,
+    sess_data: Optional[str] = None
+) -> None:
     ugc_play = await get_ugc_play(
-        cid=ugc_view_data.cid,
-        bvid=ugc_view_data.bvid,
-        aid=ugc_view_data.aid,
+        cid=page_data.cid,
+        bvid=page_data.bvid,
+        aid=page_data.aid,
         fnval=FormatNumberValue.get_dash_full_fnval(),
         sess_data=sess_data
     )
     if ugc_play.data is None:
         raise
     video, *_ = ugc_play.data.dash.video
-    source_url = video.base_url
 
-    file_p = dir_p.joinpath(f'{ugc_view_data.cid}.mp4')
-    await download_resource(url=source_url, file=str(file_p))
+    file_p = dir_path.joinpath(f'{page_data.cid}.mp4')
+    await download_resource(url=video.base_url, file=str(file_p))
