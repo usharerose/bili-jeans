c85f509847152d774f22469ce70ab62c6f16ca4f
Chaojie Yan <ushareroses@gmail.com>
2025-01-25 18:26:52 +0800

    feat: filter video by Quality Number

--- bili_jeans/app.py
+++ bili_jeans/app.py
@@ -3,9 +3,9 @@ bili-jeans application
 """
 import asyncio
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Set
 
-from .core.constants import FormatNumberValue
+from .core.constants import FormatNumberValue, QualityNumber
 from .core.download import download_resource
 from .core.factory import parse_web_view_url
 from .core.pages import get_ugc_pages
@@ -16,6 +16,8 @@ from .core.schemes import PageData
 async def run(
     url: str,
     dir_path: str,
+    qn: Optional[int] = None,
+    reverse_qn: bool = False,
     sess_data: Optional[str] = None
 ) -> None:
     dir_p = Path(dir_path)
@@ -25,7 +27,7 @@ async def run(
     pages = await get_ugc_pages(metadata, sess_data)
 
     tasks = [
-        asyncio.create_task(_download_page(page_data, dir_p, sess_data))
+        asyncio.create_task(_download_page(page_data, dir_p, qn, reverse_qn, sess_data))
         for page_data in pages
     ]
     _ = await asyncio.gather(*tasks)
@@ -34,6 +36,8 @@ async def run(
 async def _download_page(
     page_data: PageData,
     dir_path: Path,
+    qn: Optional[int] = None,
+    reverse_qn: bool = False,
     sess_data: Optional[str] = None
 ) -> None:
     ugc_play = await get_ugc_play(
@@ -45,7 +49,51 @@ async def _download_page(
     )
     if ugc_play.data is None:
         raise
-    video, *_ = ugc_play.data.dash.video
+    videos = ugc_play.data.dash.video
+
+    avail_qn_set = set([video.id_field for video in videos])
+    qn = _filter_avail_quality_number(avail_qn_set, qn, reverse_qn)
+    videos = [video for video in videos if video.id_field == qn]
+    video, *_ = videos
 
     file_p = dir_path.joinpath(f'{page_data.cid}.mp4')
     await download_resource(url=video.base_url, file=str(file_p))
+
+
+def _filter_avail_quality_number(
+    qn_set: Set[int],
+    qn: Optional[int] = None,
+    reverse_qn: bool = False
+) -> int:
+    """
+    filter out the target quality number
+    1. when no declared quality number, choose one from qn_set
+       if reverse_qn is True, prefer the greatest one which represents the highest quality
+       else the least one
+    2. when given quality number doesn't match anyone in qn_set
+       find the nearest one which lesser first, then greater one
+    3. when given quality number exists in qn_set, return it
+    """
+    if len(qn_set) <= 0:
+        raise ValueError('No alternative quality numbers')
+
+    if qn is None:
+        avail_qns = sorted(list(qn_set), reverse=reverse_qn)
+        qn, *_ = avail_qns
+        return qn
+
+    # if given qn doesn't match any available qn in qn_set
+    # find the nearest one which lesser first
+    if qn not in qn_set:
+        greater: Optional[int] = None
+        lesser: Optional[int] = None
+        for item in qn_set:
+            if item > qn:
+                greater = item if greater is None or greater > item else greater
+            else:
+                lesser = item if lesser is None or lesser < item else lesser
+        qn = lesser or greater
+    if qn is None:
+        raise ValueError('No available quality number')
+
+    return qn
