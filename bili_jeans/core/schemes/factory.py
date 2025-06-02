2d00314e93374ab1f5cbe6d34aaed736fbaa3901
Chaojie Yan <ushareroses@gmail.com>
2025-06-02 22:47:11 +0800

    feat: add properties for normalized page schema

--- bili_jeans/core/schemes/factory.py
+++ bili_jeans/core/schemes/factory.py
@@ -2,8 +2,14 @@
 Scheme definition of Bilibili view's metadata
 """
 from typing import Optional
+from urllib.parse import urlencode, urlparse, urlunparse
 
 from pydantic import BaseModel
+
+from ..constants import (
+    URL_WEB_HOST,
+    URL_WEB_NAMESPACE_UGC
+)
 
 
 class WebViewMetaData(BaseModel):
@@ -23,3 +29,24 @@ class PageData(BaseModel):
     title: str
     cover: str
     duration: Optional[int]     # unit is second
+    description: str
+    owner_name: str
+    # no publish date info for the pages, would fill view's here
+    pubdate: int
+
+    @property
+    def page_url(self) -> str:
+        if self.bvid is None:
+            raise ValueError('BV ID of the page is required')
+
+        unparsed_base = urlparse(URL_WEB_HOST)
+        parts = (
+            unparsed_base.scheme,
+            unparsed_base.netloc,
+            '/'.join([*(URL_WEB_NAMESPACE_UGC.split('/')), self.bvid]),
+            '',
+            urlencode({'p': self.idx}),
+            ''
+        )
+        url = urlunparse(parts)
+        return url
